d06586cfb4af5b43cb506ed11423b23800f43498
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2020-05-04 04:41:10 -0700

    more kotlin setup in gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,28 @@
-buildscript {
-  repositories {
-    mavenCentral()
-  }
-
-  dependencies {
-    classpath(kotlin("gradle-plugin", version = "1.3.72"))
-  }
-}
 plugins {
-  kotlin("jvm")
+  id 'org.jetbrains.kotlin.jvm' version '1.2.21'
+}
+
+apply plugin: 'application'
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    compile 'org.jetbrains.kotlin:kotlin-stdlib-jdk8'
+}
+
+sourceSets {
+    main.kotlin.srcDirs += 'src'
+}
+
+// Define the main startup class and jar name
+mainClassName = 'MainKt'
+archivesBaseName = 'sqs-plyground'
+
+// tell the jar which class to startup in.
+jar {
+    manifest {
+        attributes 'Main-Class': 'MainKt'
+    }
 }
