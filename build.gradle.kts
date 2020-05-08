38756e6760c065d57968753dab0590f6760520db
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2020-05-08 01:28:17 -0700

    commented out all the shadow stuff because it doesn't work with gradle 6.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+// import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
 buildscript {
     repositories {
         mavenCentral()
@@ -11,11 +13,21 @@ buildscript {
 plugins {
     application
     kotlin("jvm") version "1.3.72"
+    // id("com.github.johnrengelman.shadow") version "5.2.0"
+    // Need a resolution to https://github.com/johnrengelman/shadow/issues/336
+    // before we can add Shadow back into the build
 }
 
 application {
     mainClass.set("com.yashdalfthegray.sqsplayground.MainKt")
 }
+
+// tasks {
+//     named<ShadowJar>("shadowJar") {
+//         mainClass.set("com.yashdalfthegray.sqsplayground.MainKt")
+//         minimize()
+//    }
+// }
 
 repositories {
     mavenCentral()
