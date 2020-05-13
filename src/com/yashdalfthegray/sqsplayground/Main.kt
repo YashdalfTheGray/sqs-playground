f0eba0b60cf5354dbc38cebb62667c541e1a0ba8
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2020-05-13 02:13:46 -0700

    added some code to retrieve environment variables from the .env file

--- src/com/yashdalfthegray/sqsplayground/Main.kt
+++ src/com/yashdalfthegray/sqsplayground/Main.kt
@@ -1,5 +1,14 @@
 package com.yashdalfthegray.sqsplayground
 
+import io.github.cdimascio.dotenv.dotenv
+
 fun main(args: Array<String>): Unit {
-  println("This is a test")
+  val dotenv = dotenv {
+    ignoreIfMalformed = true
+    ignoreIfMissing = true
+  }
+
+  println(dotenv["AWS_ACCESS_KEY_ID"])
+  println(dotenv["AWS_SECRET_ACCESS_KEY"])
+  println(dotenv["AWS_DEFAULT_REGION"])
 }
