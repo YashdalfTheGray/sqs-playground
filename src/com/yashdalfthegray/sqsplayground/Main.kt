c47850967080c643b7f341daab43db618fffa483
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2020-05-14 02:06:13 -0700

    changed credentials to env in main as well

--- src/com/yashdalfthegray/sqsplayground/Main.kt
+++ src/com/yashdalfthegray/sqsplayground/Main.kt
@@ -1,14 +1,9 @@
 package com.yashdalfthegray.sqsplayground
 
-import io.github.cdimascio.dotenv.dotenv
+import com.yashdalfthegray.sqsplayground.config.Env
 
 fun main(args: Array<String>): Unit {
-  val dotenv = dotenv {
-    ignoreIfMalformed = true
-    ignoreIfMissing = true
-  }
-
-  println(dotenv["AWS_ACCESS_KEY_ID"])
-  println(dotenv["AWS_SECRET_ACCESS_KEY"])
-  println(dotenv["AWS_DEFAULT_REGION"])
+  println(Env.awsAccessKeyId)
+  println(Env.awsSecretAccessKey)
+  println(Env.awsDefaultRegion)
 }
