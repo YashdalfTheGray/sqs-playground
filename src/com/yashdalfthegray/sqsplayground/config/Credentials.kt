8bad6b28eb0bc3b3821962c18066fdee0e2a48a5
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2020-05-14 02:03:43 -0700

    created a singleton object for getting creds out of a .env file

---
+++ src/com/yashdalfthegray/sqsplayground/config/Credentials.kt
@@ -0,0 +1,20 @@
+package com.yashdalfthegray.sqsplayground.config
+
+import io.github.cdimascio.dotenv.dotenv
+
+object Credentials {
+  private val dotenv = dotenv {
+    ignoreIfMalformed = false
+    ignoreIfMissing = false
+  }
+    private get
+
+  val awsAccessKeyId: String?
+    get() = this.dotenv["AWS_ACCESS_KEY_ID"]
+
+  val awsSecretAccessKey: String?
+    get() = this.dotenv["AWS_SECRET_ACCESS_KEY"]
+
+  val awsDefaultRegion: String?
+    get() = this.dotenv["AWS_DEFAULT_REGION"]
+}
