1225742b118b756caf5f8e71283f8473e111ab68
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2020-05-15 01:24:07 -0700

    reindented for 4 spaces like it should be

--- src/com/yashdalfthegray/sqsplayground/Main.kt
+++ src/com/yashdalfthegray/sqsplayground/Main.kt
@@ -3,7 +3,7 @@ package com.yashdalfthegray.sqsplayground
 import com.yashdalfthegray.sqsplayground.config.Env
 
 fun main(args: Array<String>): Unit {
-  println(Env.awsAccessKeyId)
-  println(Env.awsSecretAccessKey)
-  println(Env.awsDefaultRegion)
+    println(Env.awsAccessKeyId)
+    println(Env.awsSecretAccessKey)
+    println(Env.awsDefaultRegion)
 }
--- src/com/yashdalfthegray/sqsplayground/config/Env.kt
+++ src/com/yashdalfthegray/sqsplayground/config/Env.kt
@@ -3,18 +3,17 @@ package com.yashdalfthegray.sqsplayground.config
 import io.github.cdimascio.dotenv.dotenv
 
 object Env {
-  private val dotenv = dotenv {
-    ignoreIfMalformed = false
-    ignoreIfMissing = false
-  }
-    private get
+    private val dotenv = dotenv {
+        ignoreIfMalformed = false
+        ignoreIfMissing = false
+    }
 
-  val awsAccessKeyId: String?
-    get() = this.dotenv["AWS_ACCESS_KEY_ID"]
+    val awsAccessKeyId: String?
+        get() = this.dotenv["AWS_ACCESS_KEY_ID"]
 
-  val awsSecretAccessKey: String?
-    get() = this.dotenv["AWS_SECRET_ACCESS_KEY"]
+    val awsSecretAccessKey: String?
+        get() = this.dotenv["AWS_SECRET_ACCESS_KEY"]
 
-  val awsDefaultRegion: String?
-    get() = this.dotenv["AWS_DEFAULT_REGION"]
+    val awsDefaultRegion: String?
+        get() = this.dotenv["AWS_DEFAULT_REGION"]
 }
