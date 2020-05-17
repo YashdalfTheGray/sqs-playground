7704e3efb5c7c907a8d6aedbd6e78620df141645
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2020-05-17 04:35:39 -0700

    refactored sanity tests a bit

--- src/com/yashdalfthegray/sqsplayground/config/SanityCheck.kt
+++ src/com/yashdalfthegray/sqsplayground/config/SanityCheck.kt
@@ -4,21 +4,22 @@ import java.lang.RuntimeException
 
 @Throws(RuntimeException::class)
 fun run() {
-    val credsFound = (Env.awsAccessKeyId != "") && (Env.awsSecretAccessKey != "");
-    val regionFound = Env.awsDefaultRegion != ""
-    val sqsQueueFound = Env.sqsQueueName != ""
-    val modeFound = Env.isConsumer || Env.isProducer
+    var message = ""
 
-    if (!credsFound) {
-        throw RuntimeException("No AWS credentials found")
+    if ((Env.awsAccessKeyId == "") || (Env.awsSecretAccessKey == "")) {
+        message = "No AWS credentials found"
     }
-    if (!regionFound) {
-        throw RuntimeException("No region defined for the application")
+    if (Env.awsDefaultRegion == "") {
+        message = "No region defined for the application"
     }
-    if (!sqsQueueFound) {
-        throw RuntimeException("This application needs an SQS queue to operate")
+    if (Env.sqsQueueName == "") {
+        message = "This application needs an SQS queue to operate"
     }
-    if (!modeFound) {
-        throw RuntimeException("The MODE environment variable was not defined")
+    if (!Env.isConsumer && !Env.isProducer) {
+        message = "The MODE environment variable was not defined"
+    }
+
+    if (message.isNotEmpty()) {
+        throw RuntimeException(message)
     }
 }
