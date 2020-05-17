e83088e2514857c4b756516ab1c884b87d52f55e
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2020-05-17 04:32:31 -0700

    added a sanity check function to the packagec

--- src/com/yashdalfthegray/sqsplayground/Main.kt
+++ src/com/yashdalfthegray/sqsplayground/Main.kt
@@ -1,12 +1,14 @@
 package com.yashdalfthegray.sqsplayground
 
-import com.yashdalfthegray.sqsplayground.config.Env
+import com.yashdalfthegray.sqsplayground.config.run as runSanityTest;
 
 fun main(args: Array<String>): Unit {
-    println(Env.awsAccessKeyId)
-    println(Env.awsSecretAccessKey)
-    println(Env.awsDefaultRegion)
-    println(Env.sqsQueueName)
-    println(Env.isConsumer)
-    println(Env.isProducer)
+    try {
+        runSanityTest()
+        println("Sanity check completed.")
+    } catch (t: Throwable) {
+        println("Application encountered error: ")
+        println(t)
+    }
+
 }
